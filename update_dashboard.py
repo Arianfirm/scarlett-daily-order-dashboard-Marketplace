878bc88f59b55be099808e7515c54e7cde3a9d48
Arianfirm <101966594+Arianfirm@users.noreply.github.com>
2026-06-07 23:24:49 +0700

    Update update_dashboard.py

--- update_dashboard.py
+++ update_dashboard.py
@@ -1,7 +1,27 @@
 import os
+import requests
 
 email = os.getenv("ACHANTO_EMAIL")
 password = os.getenv("ACHANTO_PASSWORD")
 
-print("EMAIL:", email)
-print("PASSWORD FOUND:", password is not None)
+url = "https://wms-api.anchanto.com/api/login"
+
+payload = {
+    "api_user": {
+        "email": email,
+        "password": password
+    }
+}
+
+response = requests.post(url, json=payload)
+
+print("Status:", response.status_code)
+
+data = response.json()
+
+print("Summary:", data.get("summary"))
+
+if "jwt" in data:
+    print("LOGIN SUCCESS")
+else:
+    print("LOGIN FAILED")
