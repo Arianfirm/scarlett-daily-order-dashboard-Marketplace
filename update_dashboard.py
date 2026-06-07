2e2de0f089ea5399906faab9774684251554bb74
Arianfirm <101966594+Arianfirm@users.noreply.github.com>
2026-06-07 23:28:07 +0700

    Update update_dashboard.py

--- update_dashboard.py
+++ update_dashboard.py
@@ -1,27 +1,64 @@
 import os
 import requests
+import json
 
 email = os.getenv("ACHANTO_EMAIL")
 password = os.getenv("ACHANTO_PASSWORD")
 
-url = "https://wms-api.anchanto.com/api/login"
+# LOGIN
 
-payload = {
+login_url = "https://wms-api.anchanto.com/api/login"
+
+login_payload = {
     "api_user": {
         "email": email,
         "password": password
     }
 }
 
-response = requests.post(url, json=payload)
+login_response = requests.post(login_url, json=login_payload)
 
-print("Status:", response.status_code)
+login_data = login_response.json()
 
-data = response.json()
+jwt = login_data["jwt"]
 
-print("Summary:", data.get("summary"))
+print("LOGIN SUCCESS")
 
-if "jwt" in data:
-    print("LOGIN SUCCESS")
-else:
-    print("LOGIN FAILED")
+# CREATE REPORT
+
+headers = {
+    "Authorization": f"Bearer {jwt}",
+    "Content-Type": "application/json"
+}
+
+report_payload = {
+    "report_schedule": {
+        "report_type_id": "3",
+        "report_format": "csv",
+        "report_occurrence_id": "5",
+        "mailing_list": [""]
+    },
+    "carrier_code": [],
+    "end_date": "2026-06-07",
+    "field_ids": [
+        "12","14","16","22","623","28","1220"
+    ],
+    "filters": {
+        "company_id": ["2"],
+        "campaign_code": []
+    },
+    "from_date": "2026-06-07",
+    "notification_type": "email"
+}
+
+create_url = "https://wms-api.anchanto.com/api/v1/report_schedules"
+
+response = requests.post(
+    create_url,
+    headers=headers,
+    json=report_payload
+)
+
+print("CREATE REPORT STATUS:", response.status_code)
+
+print(json.dumps(response.json(), indent=2))
